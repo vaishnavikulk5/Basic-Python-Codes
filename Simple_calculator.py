0494df481034d440b59943862d974d53307e02c9
vaishnavikulk5 <135519050+vaishnavikulk5@users.noreply.github.com>
2024-03-05 00:48:45 +0530

    Add files via upload

---
+++ Simple_calculator.py
@@ -0,0 +1,8 @@
+num1= input("Enter first number: ")
+num2=input("Enter second number: ")
+op= input("Choose the operation to be performed:\n1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n")
+if op==1:
+    print("Addition: ")
+    print(num1+num2)
+ 
+
